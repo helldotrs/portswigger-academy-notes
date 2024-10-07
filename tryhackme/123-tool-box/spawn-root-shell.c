ba470884b5b177b90e771916570dd0646857b352
Rebekah <macrosystem@proton.me>
2024-10-07 15:26:07 +0200

    Update and rename spawn-root-shell.c.so to spawn-root-shell.c

--- tryhackme/123-tool-box/spawn-root-shell.c
+++ tryhackme/123-tool-box/spawn-root-shell.c
@@ -1,4 +1,6 @@
 /*useful when sudo -l returns env_keep+=LD_PRELOAD*/
+/*compile: 
+     gcc -fPIC -shared -o shell.so spawn-root-shell.c -nostartfiles*/
 #include <stdio.h>
 #include <sys/types.h>
 #include <stdlib.h>
