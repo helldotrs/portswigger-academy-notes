94d45e47641f96f504799e4c8b961cf78d5cc5b7
Rebekah <macrosystem@proton.me>
2024-10-11 09:30:22 +0200

    Update and rename root-bash-shell.c to run-as-root.c

--- tryhackme/123-tool-box/run-as-root.c
+++ tryhackme/123-tool-box/run-as-root.c
@@ -2,5 +2,4 @@
 void main() {
     setuid(0); // Set the user ID to 0 (root)
     setgid(0); // Set the group ID to 0 (root)
-    system("/bin/bash"); // or change to command/app that you want to run
-}
+    system("my-app"); // run my-app
